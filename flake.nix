69be57c6ef0f83acb1ddd874b24c841036dd6e62
Oli Strik <strik@olii.nl>
2025-05-17 15:03:48 +0200

    init flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "dev shell for nix people";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { flake-utils, nixpkgs, ags, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = {
+          default = ags.lib.bundle {
+            src = ./.;
+            name = "astal-lab";
+            entry = "app.ts";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            (ags.packages."${system}".default.override {
+              extraPackages = [ ];
+            })
+          ];
+        };
+      });
+}
