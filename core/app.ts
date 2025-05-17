af929d6a1fc9eac13ad8622f91b268b2c1e1b113
Oli Strik <strik@olii.nl>
2025-05-17 15:31:32 +0200

    change core name

--- core/app.ts
+++ core/app.ts
@@ -3,8 +3,9 @@ import style from "./style.scss"
 import Bar from "./widget/Bar"
 
 App.start({
-    css: style,
-    main() {
-        App.get_monitors().map(Bar)
-    },
+	css: style,
+	instanceName: "astal-lab/core",
+	main() {
+		App.get_monitors().map(Bar)
+	},
 })
